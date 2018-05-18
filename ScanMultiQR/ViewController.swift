83a85677010fa653ec3a5a5ba3a6efb479af42d1
Carmine Massei <carminemassei@macbook-pro-di-carmine.local>
2018-05-18 10:04:59 +0200

    Initial Commit

---
+++ ScanMultiQR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScanMultiQR
+//
+//  Created by Carmine Massei on 18/05/18.
+//  Copyright © 2018 iOSFoundation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
